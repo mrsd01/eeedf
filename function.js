3ffda89ec5a2c8050752232883dcc47bbfb98a5c
mrsd01 <87982490+mrsd01@users.noreply.github.com>
2021-07-26 18:28:03 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,10 @@
+
+2
+3
+4
+5
+alert("TOUCH THE CAKE 🎂 AND ENJOY THE PARTY 🎶🎉🎊");
+window.addEventListener('click', function() {
+    var audio = document.getElementById("HBD");
+    audio.play();
+});
